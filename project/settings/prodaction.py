a4bc15c993bd9579b748bf8480132f8a382f7d78
Ididit4Ulin <uuuuiiiaaa@gmail.com>
2018-07-18 23:44:58 +0300

    missed brackets in prodaction.py

--- project/settings/prodaction.py
+++ project/settings/prodaction.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY_IDIDIT']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.environ['ALOWED_HOST_1']
+ALLOWED_HOSTS = [os.environ['ALOWED_HOST_1']]
 
 
 # Application definition
