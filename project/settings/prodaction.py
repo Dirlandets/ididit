81e901394eceaabaa391907cd91cabcc46b66a72
Ididit4Ulin <uuuuiiiaaa@gmail.com>
2018-07-18 23:26:49 +0300

    fixed again

--- project/settings/prodaction.py
+++ project/settings/prodaction.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY_IDIDIT']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ['ALOWED_HOST_1']
 
 
 # Application definition
